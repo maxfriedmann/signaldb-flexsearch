e64686c5e24b52bf0025657371666e0e60fbeaaf
maxfriedmann <max@smallstack.com>
2025-04-28 12:50:47 +0200

    Update

--- src/lib/SignalDb.svelte
+++ src/lib/SignalDb.svelte
@@ -1,80 +1,121 @@
 <script lang="ts">
-  import { Collection } from '@signaldb/core';
+  import { faker } from "@faker-js/faker";
+  import { Collection } from "@signaldb/core";
   import svelteReactivityAdapter from "@signaldb/svelte";
-  import { Index, Document, Encoder, Charset, Resolver, Worker } from "flexsearch";
-  import { faker } from '@faker-js/faker';
+  import { Charset, Document } from "flexsearch";
 
-  const logs = $state([]);
+  const logs: string[] = $state([]);
 
   // create posts collection
-  const posts = new Collection({ name: 'posts', reactivity: svelteReactivityAdapter });
+  const posts = new Collection({
+    name: "posts",
+    reactivity: svelteReactivityAdapter,
+  });
 
   // create index
   const index = new Document({
     document: {
       id: "id",
       store: true,
-      index: ["title", "text"]
+      index: ["title", "text"],
     },
     tokenize: "full",
-    encoder: Charset.LatinBalance
+    encoder: Charset.LatinBalance,
   });
 
   // add collection events
   posts.on("added", (e) => {
-    index.add(e.id, e)
+    index.add(e.id, e);
     logs.push("Added to search index: " + e.id);
+  });
+  posts.on("changed", (e) => {
+    index.update(e.id, e);
+    logs.push("Updated search index: " + e.id);
   });
 
   // fill collection
-  for (let i = 0; i < 5; i++){
-    posts.insert({ title: faker.company.buzzNoun(), text: faker.company.buzzPhrase(), likes: faker.number.int(100) }); 
+  for (let i = 0; i < 5; i++) {
+    posts.insert({
+      title: faker.company.buzzNoun(),
+      text: faker.company.buzzPhrase(),
+      likes: faker.number.int(100),
+    });
   }
 
-  // methods
-  let searchText = $state("");
-  let searchResults = $state(undefined);
-  function search() {
-    searchResults = index.search({query: searchText, suggest: true, enrich: true, highlight: "<span class='text-yellow-200'>$1</span>" });
+  // randomize
+  function randomize() {
+    posts.find().forEach((post) => {
+      posts.updateOne(
+        { id: post.id },
+        {
+          $set: {
+            title: faker.company.buzzNoun(),
+            text: faker.company.buzzPhrase(),
+            likes: faker.number.int(100),
+          },
+        },
+      );
+    });
   }
- 
+
+  // search
+  let searchText = $state("");
+  let searchResults: any[] = $state([]);
+  function search(text: string) {
+    searchResults = (
+      index.search({
+        query: text,
+        suggest: true,
+        enrich: true,
+        highlight: "<span class='text-yellow-200'>$1</span>",
+      }) as any[]
+    ).reduce((p, c) => {
+      return [...p, ...c.result];
+    }, []);
+  }
+  $effect(() => {
+    search(searchText);
+  });
 </script>
- 
+
+<div class="text-3xl py-4">Search</div>
 <div class="flex flex-row gap-1">
-  <input type="text" class="input input-border" bind:value={searchText}>
-  <button class="btn btn-primary" onclick={search}>Search</button>
+  <input type="text" class="input input-border" bind:value={searchText} />
 </div>
 {#if searchResults}
   <div class="flex flex-col">
-    {#each searchResults as searchResult }
-      <pre>{JSON.stringify(searchResult)}</pre>
+    {#each searchResults as searchResult}
+      <div class="flex flex-row gap-1">
+        ID: {searchResult.id} - Found: {@html searchResult.highlight}
+      </div>
     {/each}
   </div>
 {/if}
 
-<div class="text-lg">Data</div>
+<div class="text-3xl py-4">Data</div>
 <table class="table table-zebra">
-<thead>
-  <tr>
-    <th>ID</th>
-    <th>Title</th>
-    <th>Text</th>
-    <th>Likes</th>
-  <tr>
-</thead>
-<tbody>
-{#each posts.find().fetch() as post}
-  <tr>
-    <td>{post.id}</td>
-    <td>{post.title}</td>
-    <td>{post.text}</td>
-    <td>{post.likes}</td>
-    </tr>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Text</th>
+      <th>Likes</th>
+    </tr><tr> </tr></thead
+  >
+  <tbody>
+    {#each posts.find().fetch() as post}
+      <tr>
+        <td>{post.id}</td>
+        <td>{post.title}</td>
+        <td>{post.text}</td>
+        <td>{post.likes}</td>
+      </tr>
     {/each}
   </tbody>
 </table>
+<button class="btn btn-primary" onclick={randomize}>Randomize Values</button>
 
-<div class="text-lg">Logs</div>
-  {#each logs as log}
-    <pre>{log}</pre>
-  {/each}
+<div class="text-3xl py-4">Logs</div>
+{#each logs as log}
+  <pre>{log}</pre>
+{/each}
